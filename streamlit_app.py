017d987b7f7735f9401f158a7e31fcec5fba9e64
dmingues-sf <117829249+dmingues-sf@users.noreply.github.com>
2022-11-10 10:29:37 -0300

    Update streamlit_app.py
    Added a try-except in the fruit advice section

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
-   
-streamlit.header("Fruityvice Fruit Advice!")   
 
+#New Section to display fruitvyce api responde
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+   else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+   streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize the json API responde
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Show the normalized data
-streamlit.dataframe(fruityvice_normalized)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
